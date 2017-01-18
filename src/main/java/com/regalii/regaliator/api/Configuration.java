f674e14f4a645eddff918a0bb5d5da3354355060
Geoffrey Roguelon <geoffrey@regalii.com>
2017-01-18 11:45:35 -0600

    Add a Configuration.

---
+++ src/main/java/com/regalii/regaliator/api/Configuration.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2017, Grupo Regalii, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.regalii.regaliator.api;
+
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Created by Geoffrey Roguelon on 17/01/2017.
+ */
+public class Configuration {
+    static private final String CONTENT_TYPE = "application/json";
+    static private final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.RFC_1123_DATE_TIME;
+    static private final ZoneId DATE_ZONE = ZoneId.of("GMT");
+
+    private final Versions version;
+
+    private final String host;
+    private final String apiKey;
+    private final String secretKey;
+
+    private Boolean useSSL;
+
+    public Configuration(final Versions version, final String host, final String apiKey, final String secretKey) {
+        this.version = version;
+        this.host = host;
+        this.apiKey = apiKey;
+        this.secretKey = secretKey;
+
+        this.useSSL = true;
+    }
+
+    public AbstractClient buildClient() {
+        final Class<? extends AbstractClient> clientClass = version.getClient();
+
+        try {
+            final Constructor<? extends AbstractClient> constructor = clientClass.getConstructor(Configuration.class);
+            final AbstractClient client = constructor.newInstance(this);
+
+            return client;
+        } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String getAccept() {
+        return "application/vnd.regalii.v" + getVersion() + "+json";
+    }
+
+    public String getDate() {
+        return ZonedDateTime.now().withZoneSameInstant(DATE_ZONE).format(DATE_FORMAT);
+    }
+
+    public String getUserAgent() {
+        return "Regaliator Java v" + packageVersion();
+    }
+
+    public Class<? extends AbstractClient> getClient() {
+        return version.getClient();
+    }
+
+    public String getContentType() {
+        return CONTENT_TYPE;
+    }
+
+    public Versions getVersion() {
+        return version;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public Boolean isUsingSSL() {
+        return useSSL;
+    }
+
+    public void setUseSSL(final Boolean useSSL) {
+        this.useSSL = useSSL;
+    }
+
+    static public String packageVersion() {
+        final String version = Request.class.getPackage().getImplementationVersion();
+
+        return version == null ? "0.0.0-SNAPSHOT" : version;
+    }
+}
---
+++ src/test/java/com/regalii/regaliator/api/ConfigurationTest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2017, Grupo Regalii, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.regalii.regaliator.api;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Geoffrey Roguelon on 18/01/2017.
+ */
+public class ConfigurationTest {
+    static private final Versions VERSION = null;
+    static private final String HOST = "api.regalii.dev";
+    static private final String API_KEY = "key";
+    static private final String SECRET_KEY = "secret";
+
+    static private final Map<Versions, Class<? extends AbstractClient>> VERSIONS = new HashMap<>(Versions.values().length);
+
+    static {
+        VERSIONS.put(Versions.v1_5, com.regalii.regaliator.v15.Client.class);
+        VERSIONS.put(Versions.v3_0, com.regalii.regaliator.v30.Client.class);
+        VERSIONS.put(Versions.v3_1, com.regalii.regaliator.v31.Client.class);
+    }
+
+    private Configuration subject;
+
+    @Before
+    public void setSubject() {
+        this.subject = new Configuration(VERSION, HOST, API_KEY, SECRET_KEY);
+    }
+
+    @After
+    public void unsetSubject() {
+        this.subject = null;
+    }
+
+    @Test
+    public void testContructorSetsAttributesAndDefaults() {
+        Assert.assertEquals(VERSION, subject.getVersion());
+        Assert.assertEquals(HOST, subject.getHost());
+        Assert.assertEquals(API_KEY, subject.getApiKey());
+        Assert.assertEquals(SECRET_KEY, subject.getSecretKey());
+        Assert.assertTrue(subject.isUsingSSL());
+    }
+
+    @Test
+    public void testBuildClientReturnsClient() {
+        for (Map.Entry<Versions, Class<? extends AbstractClient>> pair : VERSIONS.entrySet()) {
+            final Configuration subject = new Configuration(pair.getKey(), HOST, API_KEY, SECRET_KEY);
+
+            Assert.assertEquals(pair.getValue(), subject.buildClient().getClass());
+        }
+    }
+
+    @Test
+    public void testGetAcceptReturnsString() {
+        final Configuration subject = new Configuration(Versions.randomVersion(), HOST, API_KEY, SECRET_KEY);
+
+        Assert.assertTrue(subject.getAccept().matches("^application/vnd\\.regalii\\.v[0-9]\\.[0-9]\\+json$"));
+    }
+
+    @Test
+    public void testGetDateReturnsString() {
+        Assert.assertTrue(subject.getDate().matches("^(?i)[a-z]{3}, \\d{1,2} (?i)[a-z]{3} \\d{4} \\d{2}:\\d{2}:\\d{2} GMT$"));
+    }
+
+    @Test
+    public void testGetUserAgentReturnsString() {
+        Assert.assertTrue(subject.getUserAgent().matches("^Regaliator Java v.+"));
+    }
+
+    @Test
+    public void testGetClientReturnsClient() {
+        final Configuration subject = new Configuration(Versions.randomVersion(), HOST, API_KEY, SECRET_KEY);
+
+        Assert.assertSame(subject.getClient(), subject.getVersion().getClient());
+    }
+
+    @Test
+    public void testGetContentTypeReturnsString() {
+        Assert.assertEquals("application/json", subject.getContentType());
+    }
+
+    @Test
+    public void testGetVersionReturnsVersions() {
+        final Versions version = Versions.randomVersion();
+        final Configuration subject = new Configuration(version, HOST, API_KEY, SECRET_KEY);
+
+        Assert.assertSame(version, subject.getVersion());
+    }
+
+    @Test
+    public void testSetUseSSLSetsBoolean() {
+        Assert.assertTrue(subject.isUsingSSL());
+        subject.setUseSSL(false);
+        Assert.assertFalse(subject.isUsingSSL());
+    }
+
+    @Test
+    public void testPackageVersionReturnsString() {
+        Assert.assertTrue(Configuration.packageVersion().matches("^\\d\\.\\d\\.\\d(-SNAPSHOT)?$"));
+    }
+}
